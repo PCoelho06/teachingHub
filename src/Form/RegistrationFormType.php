f60883e68bfe7f26934e5af7dafd6bfb8bff941e
PierreDSC <p.coelho@hotmail.fr>
2024-02-20 17:59:24 +0100

    Added User Entity and registration form

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'required' => true,
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'required' => false,
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'required' => false,
+            ])
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
+                'required' => true,
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les conditions d\'utilisation de la plateforme',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les deux mots de passe doivent être identiques',
+                'attr' => ['autocomplete' => 'new-password'],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répétez votre mot de passe'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit contenir au minimum {{ limit }} caractères',
+                    ]),
+                ],
+            ]);
+        // ->add('plainPassword', PasswordType::class, [
+        //     'mapped' => false,
+        //     'attr' => ['autocomplete' => 'new-password'],
+        //     'constraints' => [
+        //         new NotBlank([
+        //             'message' => 'Veuillez entrer un mot de passe',
+        //         ]),
+        //         new Length([
+        //             'min' => 8,
+        //             'minMessage' => 'Votre mot de passe doit contenir au minimum {{ limit }} caractères',
+        //         ]),
+        //     ],
+        // ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
