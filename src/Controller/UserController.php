69a33145df4cc835edacf9fc7b7017b79e4b22c4
PierreDSC <p.coelho@hotmail.fr>
2024-02-23 17:58:39 +0100

    User Profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,66 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\PasswordEditType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
     #[Route('mon-compte/mon-tableau-de-bord', name: 'user_dashboard')]
     public function dashboard(): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('user/dashboard.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('mon-compte/mon-profil', name: 'user_profile')]
+    public function profile(): Response
+    {
+        return $this->render('user/profile.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('mon-compte/changer-mot-de-passe', name: 'user_password_edit')]
+    public function editPassword(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordEditType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$userPasswordHasher->isPasswordValid($user, $form->get('current_password')->getData())) {
+                $this->addFlash(
+                    'danger',
+                    'Le mot de passe actuel est incorrect'
+                );
+            } else {
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('new_password')->getData()
+                    )
+                );
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Le mot de passe a été changé'
+                );
+            }
+        }
+
+        return $this->render('user/edit-password.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
