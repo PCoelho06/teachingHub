75d3a591b0adff5bcb79e0a5f5f34981d8ad068c
PierreDSC <p.coelho@hotmail.fr>
2024-04-19 17:34:23 +0200

    Refactor dynamic form builder in DocumentSearchType

--- src/Form/DocumentSearchType.php
+++ src/Form/DocumentSearchType.php
@@ -7,37 +7,35 @@ use App\Entity\Level;
 use App\Entity\Theme;
 use App\Entity\Subject;
 use App\Data\SearchFilters;
+use App\Repository\SubjectRepository;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormInterface;
+use Doctrine\Common\Collections\Collection;
+use Symfonycasts\DynamicForms\DependentField;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfonycasts\DynamicForms\DynamicFormBuilder;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatableMessage;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Translation\TranslatableMessage;
 
 class DocumentSearchType extends AbstractType
 {
+    private SubjectRepository $subjectRepository;
+
+    public function __construct(SubjectRepository $subjectRepository)
+    {
+        $this->subjectRepository = $subjectRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $formThemeModifier = function (FormInterface $form, Subject $subject = null): void {
-            $themes = null === $subject ? [] : $subject->getThemes();
-
-            $form->add('theme', EntityType::class, [
-                'class' => Theme::class,
-                'choices' => $themes,
-                'label' => 'Thématique',
-                'choice_label' => 'name',
-                'required' => false,
-                'attr' => [
-                    'onchange' => "this.closest('form').submit()"
-                ]
-            ]);
-        };
+        $builder = new DynamicFormBuilder($builder);
 
         $builder
             ->add('type', EntityType::class, [
@@ -45,8 +43,6 @@ class DocumentSearchType extends AbstractType
                 'class' => Type::class,
                 'choice_label' => 'name',
                 'required' => false,
-                // 'multiple' => true,
-                // 'expanded' => true,
                 'attr' => [
                     'onchange' => "this.closest('form').submit()"
                 ]
@@ -54,15 +50,6 @@ class DocumentSearchType extends AbstractType
             ->add('level', EntityType::class, [
                 'label' => 'Niveau',
                 'class' => Level::class,
-                'choice_label' => 'name',
-                'required' => false,
-                'attr' => [
-                    'onchange' => "this.closest('form').submit()"
-                ]
-            ])
-            ->add('subject', EntityType::class, [
-                'label' => 'Matière',
-                'class' => Subject::class,
                 'choice_label' => 'name',
                 'required' => false,
                 'attr' => [
@@ -104,28 +91,33 @@ class DocumentSearchType extends AbstractType
                 ]
             ]);
 
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) use ($formThemeModifier): void {
-                // this would be your entity, i.e. SportMeetup
-                $data = $event->getData();
+        $builder->addDependent('theme', ['level', 'subject'], function (DependentField $field, ?Level $level, ?Subject $subject) {
+            (!empty($level) && !empty($subject)) ?
+                $themes = array_intersect($level->getThemes()->toArray(), $subject->getThemes()->toArray())
+                : $themes = [];
 
-                $formThemeModifier($event->getForm(), $data->getSubject());
-            }
-        );
+            $field->add(EntityType::class, [
+                'label' => 'Thématique',
+                'class' => Theme::class,
+                'choices' => $themes,
+                'choice_label' => 'name',
+                'disabled' => empty($level) || empty($subject),
+                'required' => false,
+            ]);
+        });
 
-        $builder->get('subject')->addEventListener(
-            FormEvents::POST_SUBMIT,
-            function (FormEvent $event) use ($formThemeModifier): void {
-                // It's important here to fetch $event->getForm()->getData(), as
-                // $event->getData() will get you the client data (that is, the ID)
-                $subject = $event->getForm()->getData();
-
-                // since we've added the listener to the child, we'll have to pass on
-                // the parent to the callback function!
-                $formThemeModifier($event->getForm()->getParent(), $subject);
-            }
-        );
+        $builder->addDependent('subject', ['level'], function (DependentField $field, ?Level $level) {
+            $field->add(EntityType::class, [
+                'label' => 'Matière',
+                'class' => Subject::class,
+                'choices' => $level ? $level->getSubjects() : $this->subjectRepository->findAll(),
+                'choice_label' => 'name',
+                'required' => false,
+                'attr' => [
+                    'onchange' => "this.closest('form').submit()"
+                ]
+            ]);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
