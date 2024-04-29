578b30bdc3c33c47dc9f5cad25ed136cd75af04e
PierreDSC <p.coelho@hotmail.fr>
2024-04-29 16:00:06 +0200

    Add admin templates and controller for type management

---
+++ src/Controller/Admin/TypeCrudController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Type;
+use App\Form\TypeType;
+use App\Repository\TypeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+#[Route('/administration/type', name: "admin_type_")]
+class TypeCrudController extends AbstractController
+{
+    #[Route('/nouveau-type', name: 'create')]
+    #[Route('/editer-type/{id}', name: 'update')]
+    public function handle(Type $type = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $edit = true;
+        if (!$type) {
+            $type = new Type();
+            $edit = false;
+        }
+        $form = $this->createForm(TypeType::class, $type);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($type);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin_type_read');
+        }
+        return $this->render('admin/type/handle.html.twig', [
+            'form' => $form->createView(),
+            'edit' => $edit,
+        ]);
+    }
+
+    #[Route('/', name: 'read')]
+    public function read(TypeRepository $typeRepository): Response
+    {
+        $types = $typeRepository->findAll();
+        return $this->render('admin/type/read.html.twig', [
+            'types' => $types,
+        ]);
+    }
+
+    #[Route('/supprimer-type/{id}', name: 'delete')]
+    public function delete(Type $type, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($type);
+        $entityManager->flush();
+        return $this->redirectToRoute('admin_type_read');
+    }
+}
---
+++ src/Controller/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/administration', name: "admin_")]
+#[IsGranted('ROLE_ADMIN')]
+class AdminController extends AbstractController
+{
+    #[Route('/dashboard', name: 'dashboard')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
